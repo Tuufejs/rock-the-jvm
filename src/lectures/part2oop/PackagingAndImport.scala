6ceabeee7990fcc3fc1796dce2b2855bd68d1384
tuufejs <vratahavlik@gmail.com>
2020-06-07 21:24:10 +0200

    packages

--- src/lectures/part2oop/PackagingAndImport.scala
+++ src/lectures/part2oop/PackagingAndImport.scala
@@ -1,6 +1,15 @@
 package lectures.part2oop
 
-import playground.Cinderella
+
+
+import playground.{Cinderella, PrinceCharming}
+//import playground._
+//import playground.{Cinderella => Princess, PrinceCharming} // renaming cinderella to princess for uses
+// useful when I need to import more clases with the saem name
+
+import java.util.Date
+import java.sql.{Date=> SqlDate}
+
 
 object PackagingAndImport extends App{
   // package members are accessible by their simple names
@@ -13,5 +22,18 @@ object PackagingAndImport extends App{
   //matching folder structure
 
   // package object
+  sayHello
+  println(speedOfLight)
 
+  // imports
+  val prince = new PrinceCharming
+
+  // 1. use FQ names
+  val date = new Date
+  // val sqlDate = new java.sql.Date(2018,3,20)
+  val sqlDate = new SqlDate(2018,3,20)
+
+  //default imports
+  // java.lang / String, Object, Exceptions
+  //scala / Int, Nothing, Functions
 }
--- src/lectures/part2oop/package.scala
+++ src/lectures/part2oop/package.scala
@@ -2,4 +2,7 @@ package lectures
 
 package object part2oop {
 
+  def sayHello: Unit = println("Hello Scal")
+  val speedOfLight = 29994216
+
 }
